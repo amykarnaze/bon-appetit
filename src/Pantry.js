ce0c9715b18651a52cc1b811610d5992ef39bd88
Tyler Haglund <relyt4me@gmail.com>
2020-07-09 18:57:57 -0600

    Create test for hasAllIngredients

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,10 +1,24 @@
 class Pantry {
-  constructor() {
-    this.ingredients = [];
+  constructor(ingredients) {
+    this.ingredients = ingredients;
   }
-
+  hasAllIngredients(ingredientList1) {
+    //return true or false
+    // check each item in the ingredient list against the pantry
+    // if the ingredients in the pantry has an ingredient, whose number matches the ingredient id
+    // check to see if the panty ingredient amount is greater than or equal to the ingredient quantity
+    // if all of the ingredients in the list are true for the above statement
+    // return true
+    //otherwise false
+    return ingredientList1.every((ingredientNeeded) => {
+      const foundIngredient = this.ingredients.find((pantryIngredient) => {
+        return pantryIngredient.ingredient === ingredientNeeded.id;
+      });
+      return foundIngredient.amount >= ingredientNeeded.quantity.amount;
+    });
+  }
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = Pantry;
+  module.exports = Pantry;
 }
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -2,25 +2,57 @@
 const chai = require('chai');
 
 const expect = chai.expect;
-var Pantry = require('../src/Pantry')
+var Pantry = require('../src/Pantry');
 // var User = require('../src/User')
 
 describe('Pantry', function () {
   let pantry;
-  beforeEach(function() {
+  beforeEach(function () {
     pantry = new Pantry();
   });
 
   it('should be a function', function () {
     expect(Pantry).to.be.a('function');
-});
+  });
 
-it('should be an instance of Pantry', function() {
+  it('should be an instance of Pantry', function () {
     expect(pantry).to.be.an.instanceof(Pantry);
-});
+  });
 
-it('should have a property of ingredients', function() {
+  it('should have a property of ingredients', function () {
     expect(pantry.ingredients).to.deep.equal([]);
   });
 
+  it('should tell me if it has the ingrediets in a given list of ingredients', function () {
+    // need to build a function that  when given an array of ingredients checks them against its own ingredients and returns a boolean if it has them all
+
+    // class Ingredient {
+    //   constructor(ingredient) {
+    //     this.id = ingredient.id;
+    //     this.name = ingredient.name;
+    //     this.estimatedCostInCents = ingredient.estimatedCostInCents;
+    //   }
+    // }
+
+    const ingredientList1 = [
+      {
+        id: 20081,
+        quantity: {
+          amount: 1.5,
+          unit: 'c',
+        },
+      },
+    ];
+
+    const mypantry = new Pantry([
+      {
+        amount: 4,
+        ingredient: 20081,
+      },
+    ]);
+
+    //have the panty with ingredients
+    //create a list of ingredients1 that i know the pantry has
+    expect(mypantry.hasAllIngredients(ingredientList1)).to.equal(true);
+  });
 });
