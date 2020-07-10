124be7a1004ad6060358917477584431a86b7c57
Amy Karnaze <akarnaze@gmail.com>
2020-07-10 16:57:48 -0600

    Fix merge conflicts

---
+++ src/User.js
@@ -0,0 +1,120 @@
+<<<<<<< Updated upstream
+=======
+/* eslint-disable */
+
+const ingredientsData = require('../data/ingredients');
+
+const userData = require('../data/users.js')
+class User {
+  constructor(name, id, pantry) {
+    this.name = name;
+    this.id = id;
+    this.pantry = pantry;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+  }
+
+  addFavoriteRecipe(id) {
+    if (!this.favoriteRecipes.includes(id)) {
+      this.favoriteRecipes.push(id)
+    }
+    // issue # 23
+  }
+
+  removeFavoriteRecipe(id) {
+    let item = this.favoriteRecipes.indexOf(id);
+    this.favoriteRecipes.splice(item, 1)
+    // #19
+  }
+
+  addRecipesToCook(id) {
+    if (!this.recipesToCook.includes(id)) {
+      this.recipesToCook.push(id)
+    }
+    //#20
+  }
+
+  removeRecipesToCook(id) {
+    let item = this.recipesToCook.indexOf(id);
+    this.recipesToCook.splice(item, 1)
+  }
+  // #21
+
+//search recipesToCook OR favoriteRecipes by type
+findRecipesByType(savedRecipes, tagName) {
+  return savedRecipes.filter( recipe => {
+    return recipe.tags.includes(tagName);
+  });
+}
+
+findRecipesByName(savedRecipes, name) {
+  return savedRecipes.filter(recipe => {
+    return recipe.name.includes(name)
+  });
+}
+
+// findRecipesByIngredient(savedRecipes, ingredientName) {
+//   let ingredientId;
+//   ingredientsData.forEach(ingredient => {
+//     if (ingredientName.includes(ingredient.name)) {
+//       ingredientId = ingredient.id;
+//     }
+//   })
+//     console.log(ingredientId);
+//     let matchedRecipes = [];
+//     savedRecipes.forEach(recipe => {
+//       recipe.ingredients.forEach(ingred => {
+//         if (ingred.id === ingredientId) {
+//           matchedRecipes.push(recipe);
+//         }
+//       })
+//     })
+//     // console.log(matchedRecipes)
+//     return matchedRecipes;
+//     // search savedRecipes for ingredients id
+//     }
+//     }
+findRecipesByIngredient(savedRecipes, ingredientName) {
+  let ingredientId;
+  ingredientsData.forEach(ingredient => {
+    if (ingredientName.includes(ingredient.name)) {
+      ingredientId = ingredient.id;
+    }
+  })
+      const matchedRecipes = savedRecipes.reduce((acc, recipe) => {
+        recipe.ingredients.forEach(currentIngredient => {
+            if (currentIngredient.id === ingredientId) {
+              acc.push(recipe)
+            }
+         })
+      return acc;
+    }, []);
+    // console.log(matchedRecipes)
+        return matchedRecipes;
+  }
+
+  // do search last
+  // function name that takes in the same parameters
+// invoke each method
+// 
+}
+
+// search recipesToCook by name OR ingredient
+// ingredients in data only have id
+// recipesToCook and favorites contain 
+// ingredients in pantry
+// ingredients in recipe
+// ingredients class
+
+// user.pantry.ingredients
+
+// A User should be able to determine whether they have sufficient ingredients in their pantry to cook a recipe
+// if they DO NOT: see pantry stories
+// return a list of ingredients they need to buy and how much it will cost
+// (note - User should be able to do this for at least one recipe)
+// push into recipesToCook array
+
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}
+>>>>>>> Stashed changes
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,14 +1,23 @@
 /* eslint-disable */
 const chai = require('chai');
-
+const usersData = require('../data/users.js');
 const expect = chai.expect;
-var Pantry = require('../src/Pantry')
+var Pantry = require('../src/Pantry');
+var Recipe = require('../src/Recipe');
+const recipeData = require('../data/recipes.js');
+
 // var User = require('../src/User')
 
 describe('Pantry', function () {
   let pantry;
+  let recipe;
   beforeEach(function() {
+<<<<<<< Updated upstream
     // user = new Pantry();
+=======
+    pantry = new Pantry(usersData[0].pantry);
+    recipe = new Recipe(recipeData[0]);
+>>>>>>> Stashed changes
   });
 
   it('should be a function', function () {
@@ -19,8 +28,24 @@ it('should be an instance of User', function() {
     expect(pantry).to.be.an.instanceof(Pantry);
 });
 
-it('should have a property of ingredients', function() {
-    expect(pantry.ingredients).to.deep.equal([]);
+it('should have a property of userPantry', function() {
+    expect(pantry.userPantry).to.deep.equal(usersData[0].pantry);
   });
 
+  describe('findMissingIngredients', function () {
+    it('should find missing ingredients', () => {
+    // pantry.findMissingIngredients(recipe);
+    const missingIngredients = [{
+      "id": "1",
+      "quantity": {
+        "amount": 1.5,
+        "unit": "c"
+      }
+    }];
+    recipe.ingredients = missingIngredients;
+      expect(pantry.findMissingIngredients(recipe)).to.deep.equal(missingIngredients);
+  })
+
+  // todo write happy path for when ingredients are found
+  })
 });
