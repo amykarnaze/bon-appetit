cc0833f564413fd503d0f07fae7d2ecd807e9210
Tyler Haglund <relyt4me@gmail.com>
2020-07-12 12:51:15 -0600

    Make drop down loop over users and add names

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,24 @@
-const usersData = require('../data/users.js');
-const ingredientsData = require('../data/ingredients');
-const recipesData = require('../data/recipes');
+// const usersData = require('../data/users.js');
+// const ingredientsData = require('../data/ingredients');
 
-// module.exports = {
-//   search,
-// };
+// const usersData = require('../data/users');
+
+// const recipesData = require('../data/recipes');
+window.onload = setup();
+
+function setup() {
+  console.log('I got here');
+  populateUserSelector();
+}
+
+function populateUserSelector() {
+  const userSelector = document.querySelector('.user-selector');
+  let userSelectorInnerHTML = '';
+  usersData.forEach((user) => {
+    userSelectorInnerHTML =
+      userSelectorInnerHTML +
+      `<option value="${user.name}" selected>${user.name}</option>
+      `;
+  });
+  userSelector.innerHTML = userSelectorInnerHTML;
+}
